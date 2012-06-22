391ddb240d63ef0b296f96319aafbb35e702c886
rynosaurus1 <rynosaurus1@gmail.com>
2012-06-22 05:06:07 +0000

    Initial commit.

---
+++ lua/ulx/modules/cl/youtube_music_player_cl.lua
@@ -0,0 +1,473 @@
+-- ULX YouTube music player - Allows admins to play audio from YouTube videos on the server.
+local PlayerSize = { x = 268, y = 19 }
+local PlayerPos = { x = ( ScrW() / 2 ) - ( PlayerSize.x / 2 ), y = 2 }
+
+local reloading
+if ULXSongPlayerPanel != nil and type( ULXSongPlayerPanel ) == "Panel" then
+	ULXSongPlayerPanel:Remove()
+	reloading = true
+end
+
+if ULXSongPlayer != nil and type( ULXSongPlayer ) == "Panel" then
+	ULXSongPlayer:Remove()
+	reloading = true
+end
+
+local panel
+local SongPlayer
+local function songplayer_init( authed_ply )
+
+	ULXSongPlayerPanel = vgui.Create( "DPanel" )
+	panel = ULXSongPlayerPanel
+	panel:SetPos( PlayerPos.x, PlayerPos.y )
+	panel:SetSize( PlayerSize.x, PlayerSize.y )
+	panel:SetVisible( false )
+
+	ULXSongPlayer = vgui.Create( "HTML", panel )
+	SongPlayer = ULXSongPlayer
+	SongPlayer:SetSize( 3 + 55 + 1 + PlayerSize.x + 1 + 28 + 1 + 1 + 30 + 1, PlayerSize.y + 8 ) -- Make it a little bigger than the panel, to cut off the "watch later" button, the pause/play button, and the seek bar.
+	SongPlayer:SetPos( -3 - 55 - 1, -4 ) -- Offset it a little to hide the pause/play button.
+	SongPlayer:SetMouseInputEnabled( false )
+	SongPlayer:SetVisible( false )
+
+end
+hook.Add( ULib.HOOK_LOCALPLAYERREADY, "ULXSongPlayerInit", songplayer_init )
+
+if ( reloading ) then
+	songplayer_init()
+	reloading = false
+end
+
+local enabled = CreateConVar( "ulx_songplayer_enable", "1", FCVAR_ARCHIVE, "Enables/disables the ULX YouTube music player client-side" )
+local quality_help =
+[[Sets the ULX YouTube music player video quality client-side (if the quality isn't supported by the video, it will use the closest one available)
+Possible values: 240p, 360p,  480p,  720p,  1080p]]
+local quality = CreateConVar( "ulx_songplayer_quality", "480p", FCVAR_ARCHIVE, quality_help )
+
+local function setPlayerVisible( b ) -- Helper function for setting the visibility of both the DPanel and the HTML conrol simultaneously
+	panel:SetVisible( b )
+	SongPlayer:SetVisible( b )
+end
+
+local function playerFade( fading_in, fade_time )
+
+	if panel:IsVisible() and fading_in then return end -- The player is already visible, no need to fade in.
+	if not panel:IsVisible() and not fading_in then return end -- The player is already hidden, no need to fade out.
+
+	fade_time = fade_time or 1
+	local start = CurTime()
+
+	if fading_in then
+		panel:SetAlpha( 0 )
+		setPlayerVisible( true )
+	end
+
+	local function playerFadeHook()
+
+		local alpha
+		local ftime = CurTime() - start
+
+		local cur_alpha = panel:GetAlpha()
+
+		if ( fading_in and cur_alpha < 255 ) or ( not fading_in and cur_alpha > 0 ) then
+
+			if fading_in then -- Fading in
+
+				local alpha_mul = ftime / fade_time -- 0 to 1
+				alpha = 255 * alpha_mul
+
+			else -- Fading out
+
+				local alpha_mul = ftime / fade_time -- 0 to 1
+				local alpha_mul = 1 - alpha_mul -- Reverse, 1 to 0
+				alpha = 255 * alpha_mul
+			end
+
+			if alpha > 255 then
+				alpha = 255
+			elseif alpha < 0 then
+				alpha = 0
+			end
+
+			panel:SetAlpha( alpha )
+
+		else -- Done fading
+
+			if not fading_in then -- Was fading out
+				setPlayerVisible( false )
+				panel:SetAlpha( 255 )
+			end
+			hook.Remove( "Think", "YouTubePlayerFade" )
+
+		end
+	end
+
+	hook.Add( "Think", "YouTubePlayerFade", playerFadeHook )
+end
+
+local function playerFadeIn( fade_time )
+	playerFade( true, fade_time )
+end
+local function playerFadeOut( fade_time )
+	playerFade( false, fade_time )
+end
+
+function ulx.play_vid_hook( um )
+
+	if enabled:GetBool() == false then return end
+
+	local video_id = um:ReadString()
+
+	SongPlayer:SetHTML( "" )
+
+	timer.Simple( 0.3, function()
+
+		http.Get( "http://gdata.youtube.com/feeds/api/videos/" .. video_id .. "?v=2", "", function( contents )
+
+			local video_length = ulx.getVideoLengthFromHTTPData( contents )
+
+			if not video_length then
+				print( "YouTube music player: Error retrieving video length for player auto-hide. Type ulx stopvid to manually hide the player bar." )
+				return
+			end
+
+			timer.Create( "ulx_hidevid", video_length + 3, 1, function()
+				playerFadeOut()
+			end )
+
+		end )
+
+		SongPlayer:OpenURL( "http://ryno-saurus.github.com/ulx_youtubemusicplayer/host.html?v=" .. video_id .. "&quality=" .. quality:GetString() )
+
+		timer.Simple( 0.5, playerFadeIn )
+
+	end )
+
+end
+usermessage.Hook( "ulx_playvid", ulx.play_vid_hook )
+
+function ulx.stop_vid_hook()
+	SongPlayer:SetHTML( "" )
+
+	timer.Simple( 0.3, function()
+
+		timer.Simple( 0.5, playerFadeOut )
+
+		if enabled:GetBool() == false then return end
+
+		SongPlayer:OpenURL( "http://ryno-saurus.github.com/ulx_youtubemusicplayer/host.html?quality=" .. quality:GetString() )
+
+	end )
+
+	timer.Destroy( "ulx_hidevid" )
+end
+usermessage.Hook( "ulx_stopvid", ulx.stop_vid_hook )
+
+local SongCount
+function ULXMusicPlayerRefresh()
+	if not ULXSongPlayerFrame or not ULXSongPlayerFrame:IsVisible() then return end
+	SongCount = 0
+	ULXSongPlayerFrame:SetTitle( "YouTube music player by RynO-SauruS *Refreshing*" )
+	ULXSongPlayerList:Clear()
+	RunConsoleCommand( "_ulx_songplayer", "retrievesongs" )
+end
+
+function ULXReceiveSong( um )
+
+	local video_id = um:ReadString()
+	local title = um:ReadString()
+	local length = um:ReadLong()
+	local length_minutes = math.floor( length / 60 )
+	local length_seconds = length - ( 60 * length_minutes )
+	if length_seconds < 10 then length_seconds = "0" .. length_seconds end
+
+	ULXSongPlayerList:AddLine( title, video_id, length_minutes .. ":" .. length_seconds )
+
+	SongCount = SongCount + 1
+
+end
+usermessage.Hook( "ULXTransferSong", ULXReceiveSong )
+
+function ULXRecieveSongsDone()
+	local CountString
+	if SongCount == 1 then
+		CountString = "(1 song)"
+	else
+		CountString = "(" .. SongCount .. " songs)"
+	end
+	ULXSongPlayerFrame:SetTitle( "YouTube music player by RynO-SauruS " .. CountString )
+	ULXSongPlayerList:SortByColumn( 1 )
+end
+usermessage.Hook( "ULXTransferSongsDone", ULXRecieveSongsDone )
+
+function ULXOpenMusicPlayer()
+
+	ULXSongPlayerFrame = vgui.Create( "DFrame" )
+	local frame = ULXSongPlayerFrame
+	frame:MakePopup()
+	frame:SetTitle( "YouTube music player by RynO-SauruS" )
+	frame:ShowCloseButton( false )
+	frame:SetDeleteOnClose( true )
+	frame:SetKeyboardInputEnabled( false )
+	frame:SetSizable( true )
+
+	ULXSongPlayerList = vgui.Create( "DListView", frame )
+	local list = ULXSongPlayerList
+	list:SetMultiSelect( false )
+	list:AddColumn( "Title" )
+	list:AddColumn( "Video ID" ):SetFixedWidth( 0 ) -- No need to show this (makes more room for the title), if they want the video ID they can right click-> Copy URL to clipboard
+	list:AddColumn( "Length" ):SetFixedWidth( 38 )
+	list.DoDoubleClick = function( parent, index, line )
+		if ULib.ucl.query( LocalPlayer(), "ulx playvid" ) then
+			RunConsoleCommand( "_ulx_songplayer", "playvid", line:GetValue( 2 ) )
+		else
+			RunConsoleCommand( "_ulx_songplayer", "playvidcl", line:GetValue( 2 ) )
+		end
+	end
+	list.OnRowRightClick = function( panel, line )
+		local menu = vgui.Create( "DMenu" )
+		menu:SetPos( gui.MousePos() )
+		menu:AddOption( "Play for all", function()
+			RunConsoleCommand( "_ulx_songplayer", "playvid", list:GetLine( line ):GetValue( 2 ) )
+		end )
+		menu:AddOption( "Play client-side", function()
+			RunConsoleCommand( "_ulx_songplayer", "playvidcl", list:GetLine( line ):GetValue( 2 ) )
+		end )
+		menu:AddSpacer()
+		menu:AddOption( "Copy URL to clipboard", function()
+			SetClipboardText( "http://www.youtube.com/watch?v=" .. list:GetLine( line ):GetValue( 2 ) )
+		end )
+		menu:AddSpacer()
+		menu:AddOption( "Remove", function()
+			local confirmframe = vgui.Create( "DFrame", frame )
+			confirmframe:SetTitle( "Confirm remove" )
+			confirmframe:ShowCloseButton( false )
+			confirmframe:SetDeleteOnClose( true )
+			confirmframe:SetSize( 300, 22 + 100 )
+			confirmframe:Center()
+			confirmframe:MakePopup()
+
+			local label = vgui.Create( "DLabel", confirmframe )
+			label:SetPos( 10, 22 + 10 )
+			label:SetWide( 280 )
+			label:SetText( "Are you sure you want to remove this video?" )
+
+			local applybutton = vgui.Create( "DButton", confirmframe )
+			applybutton:SetPos( 10, 22 + 55 )
+			applybutton:SetSize( 135, 35 )
+			applybutton:SetText( "Remove" )
+			applybutton.DoClick = function()
+				RunConsoleCommand( "ulx", "removevid", "http://www.youtube.com/watch?v=" .. list:GetLine( line ):GetValue( 2 ) ) -- The server sends the refresh command back once it's removed.
+				confirmframe:Close()
+			end
+
+			local cancelbutton = vgui.Create( "DButton", confirmframe )
+			cancelbutton:SetPos( 155, 22 + 55 )
+			cancelbutton:SetSize( 135, 35 )
+			cancelbutton:SetText( "Cancel" )
+			cancelbutton.DoClick = function() confirmframe:Close() end
+		end )
+		menu:AddOption( "Rename", function()
+			local textframe = vgui.Create( "DFrame", frame )
+			textframe:SetTitle( "Rename video" )
+			textframe:ShowCloseButton( false )
+			textframe:SetDeleteOnClose( true )
+			textframe:SetSize( 300, 22 + 100 )
+			textframe:Center()
+			textframe:MakePopup()
+
+			local textbox = vgui.Create( "DTextEntry", textframe )
+			textbox:SetPos( 10, 22 + 10 )
+			textbox:SetWide( 280 )
+			textbox:SetText( list:GetLine( line ):GetValue( 1 ) )
+			textbox:RequestFocus()
+
+			local function renameVid()
+				local new_title = textbox:GetValue():gsub( "\"", "<quote>" ) -- This is to fix quotation marks being replaced with ' by RunConsoleCommand.
+				RunConsoleCommand( "ulx", "renamevid", "http://www.youtube.com/watch?v=" .. list:GetLine( line ):GetValue( 2 ), new_title )
+				textframe:Close()
+			end
+			textbox.OnEnter = renameVid
+
+			local applybutton = vgui.Create( "DButton", textframe )
+			applybutton:SetPos( 10, 22 + 55 )
+			applybutton:SetSize( 135, 35 )
+			applybutton:SetText( "Apply" )
+			applybutton.DoClick = renameVid
+
+			local cancelbutton = vgui.Create( "DButton", textframe )
+			cancelbutton:SetPos( 155, 22 + 55 )
+			cancelbutton:SetSize( 135, 35 )
+			cancelbutton:SetText( "Cancel" )
+			cancelbutton.DoClick = function() textframe:Close() end
+		end )
+		menu:Open()
+	end
+
+	local playbutton = vgui.Create( "DButton", frame )
+	playbutton:SetText( "Play for all" )
+	playbutton.DoClick = function()
+		if #list:GetSelected() <= 0 then return end
+		RunConsoleCommand( "_ulx_songplayer", "playvid", list:GetSelected()[ 1 ]:GetValue( 2 ) )
+	end
+
+	local stopbutton = vgui.Create( "DButton", frame )
+	stopbutton:SetText( "Stop for all" )
+	stopbutton.DoClick = function()
+		RunConsoleCommand( "ulx", "stopvidall" )
+	end
+
+	local refreshbutton = vgui.Create( "DButton", frame )
+	refreshbutton:SetText( "Refresh" )
+	refreshbutton.DoClick = ULXMusicPlayerRefresh
+
+	local closebutton = vgui.Create( "DButton", frame )
+	closebutton:SetText( "Close" )
+	closebutton.DoClick = function()
+		closebutton:GetParent():Close()
+	end
+
+	local playclbutton = vgui.Create( "DButton", frame )
+	playclbutton:SetText( "Play client-side" )
+	playclbutton.DoClick = function()
+		if #list:GetSelected() <= 0 then return end
+		RunConsoleCommand( "_ulx_songplayer", "playvidcl", list:GetSelected()[ 1 ]:GetValue( 2 ) )
+	end
+
+	local stopclbutton = vgui.Create( "DButton", frame )
+	stopclbutton:SetText( "Stop client-side" )
+	stopclbutton.DoClick = function()
+		RunConsoleCommand( "ulx", "stopvid" )
+	end
+
+	local addvidbutton = vgui.Create( "DButton", frame )
+	addvidbutton:SetText( "Add video" )
+	addvidbutton.DoClick = function()
+		local textframe = vgui.Create( "DFrame", frame )
+		textframe:SetTitle( "Add video" )
+		textframe:ShowCloseButton( false )
+		textframe:SetDeleteOnClose( true )
+		textframe:SetSize( 300, 22 + 100 )
+		textframe:Center()
+		textframe:MakePopup()
+
+		local label = vgui.Create( "DLabel", textframe )
+		label:SetPos( 10, 22 + 5 )
+		label:SetSize( 280, 15 )
+		label:SetText( "URL" )
+
+		local textbox = vgui.Create( "DTextEntry", textframe )
+		textbox:SetPos( 10, 22 + 25 )
+		textbox:SetWide( 280 )
+		textbox:RequestFocus()
+
+		local function addVid()
+			RunConsoleCommand( "ulx", "savevid", textbox:GetValue() )
+			textframe:Close()
+		end
+		textbox.OnEnter = addVid
+
+		local applybutton = vgui.Create( "DButton", textframe )
+		applybutton:SetPos( 10, 22 + 55 )
+		applybutton:SetSize( 135, 35 )
+		applybutton:SetText( "Add" )
+		applybutton.DoClick = addVid
+
+		local cancelbutton = vgui.Create( "DButton", textframe )
+		cancelbutton:SetPos( 155, 22 + 55 )
+		cancelbutton:SetSize( 135, 35 )
+		cancelbutton:SetText( "Cancel" )
+		cancelbutton.DoClick = function() textframe:Close() end
+	end
+
+	local addcurvidbutton = vgui.Create( "DButton", frame )
+	addcurvidbutton:SetText( "Add current video" )
+	addcurvidbutton:SetTooltip( "Add currently playing/last played video" )
+	addcurvidbutton.DoClick = function()
+		RunConsoleCommand( "ulx", "savecurvid" )
+	end
+
+	local label = vgui.Create( "DLabel", frame )
+
+	if ULib.ucl.query( LocalPlayer(), "ulx playvid" ) then
+		label:SetText( "Double click = play for all" )
+	else
+		label:SetText( "Double click = play client-side" )
+	end
+
+	local function updateText( ply )
+		if not ULXSongPlayerFrame or not ULXSongPlayerFrame:IsVisible() then return end
+		if not ValidEntity( ply ) or ply ~= LocalPlayer() then return end
+		if ULib.ucl.query( LocalPlayer(), "ulx playvid" ) then
+			label:SetText( "Double click = play for all" )
+		else
+			label:SetText( "Double click = play client-side" )
+		end
+	end
+	hook.Add( ULib.HOOK_UCLAUTH, "ULXMusicPlayerUpdateText", updateText )
+
+	local volume = vgui.Create( "DNumSlider", frame )
+	volume:SetText( "Volume" )
+	volume:SetMin( 0 )
+	volume:SetMax( 100 )
+	volume:SetDecimals( 0 )
+
+	volume.OnValueChanged = function( panel, value )
+		SongPlayer:RunJavascript( "ytplayer.setVolume( " .. value .. " )" )
+	end
+
+	local OldPerformLayout = frame.PerformLayout
+
+	frame.PerformLayout = function()
+
+		OldPerformLayout( frame )
+
+		local frameX, frameY = frame:GetSize()
+		frameY = frameY - 22
+
+		local buttonX = ( frameX - 50 ) / 4
+		local buttonY = 40
+
+		playbutton:SetPos( 10, 22 + 10 )
+		playbutton:SetSize( buttonX, buttonY )
+
+		stopbutton:SetPos( frameX - 10 - buttonX - 10 - buttonX - 10 - buttonX, 22 + 10 )
+		stopbutton:SetSize( buttonX, buttonY )
+
+		refreshbutton:SetPos( frameX - 10 - buttonX - 10 - buttonX, 22 + 10 )
+		refreshbutton:SetSize( buttonX, buttonY )
+
+		closebutton:SetPos( frameX - 10 - buttonX, 22 + 10 )
+		closebutton:SetSize( buttonX, buttonY )
+
+		playclbutton:SetPos( 10, 22 + 10 + buttonY + 10 )
+		playclbutton:SetSize( buttonX, buttonY )
+
+		stopclbutton:SetPos( frameX - 10 - buttonX - 10 - buttonX - 10 - buttonX, 22 + 10 + buttonY + 10 )
+		stopclbutton:SetSize( buttonX, buttonY )
+
+		addvidbutton:SetPos( frameX - 10 - buttonX - 10 - buttonX, 22 + 10 + buttonY + 10 )
+		addvidbutton:SetSize( buttonX, buttonY )
+
+		addcurvidbutton:SetPos( frameX - 10 - buttonX, 22 + 10 + buttonY + 10 )
+		addcurvidbutton:SetSize( buttonX, buttonY )
+
+		label:SetPos( 10, 22 + 10 + buttonY + 10 + buttonY + 5 )
+		label:SetSize( ( frameX / 2 ) - 15, 35 )
+
+        local sliderX = math.min( 150, ( frameX / 2 ) - 15 )
+
+		volume:SetPos( frameX - 10 - sliderX, 22 + 10 + buttonY + 10 + buttonY + 5 )
+		volume:SetSize( sliderX, 35 )
+
+		list:SetPos( 10, 22 + 10 + buttonY + 10 + buttonY + 5 + 35 + 5 )
+		list:SetSize( frameX - 20, frameY - ( 10 + buttonY + 10 + buttonY + 5 + 35 + 5 ) - 10 )
+
+	end
+
+	frame:SetSize( math.Clamp( 580, 0, ScrW() ), math.Clamp( 22 + 800, 0, ScrH() ) )
+	frame:Center()
+
+	ULXMusicPlayerRefresh()
+
+end
---
+++ lua/ulx/modules/sh/youtube_music_player_sh.lua
@@ -0,0 +1,341 @@
+-- ULX YouTube Jukebox - Allows admins to play audio from YouTube videos on the server.
+local ulx_cmd_category = "ULX YouTube Jukebox"
+
+local current_vid = {}
+if CLIENT then current_vid = nil end
+
+--[[
+	This function attempts to find and return the video ID in a YouTube video URL.
+	Returns nil on failure.
+]]
+function ulx.getVideoIDFromURL( url )
+	if url:len() <= 0 then return end
+
+	local _, v_end = url:find( "v=", 1, true )
+	if not v_end then return end
+
+	local video_id = url:sub( v_end + 1, v_end + 11 ) -- 11 = Length of all YouTube video ID's
+	if video_id and video_id:len() ~= 11 then return end
+
+	return video_id
+end
+
+--[[
+	This function attempts to find and return the video title in data from an HTTP request for YouTube video info.
+	Returns "<unknown_title>" on failure.
+]]
+function ulx.getVideoTitleFromHTTPData( data )
+	local video_title
+
+	if data:len() <= 0 then
+		print( "ULX YouTube music player: Error retrieving video title" )
+		video_title = "<unknown_title>"
+	else
+		local _, title_tag1_end = data:find( "<media:title type='plain'>", 1, true )
+		local title_tag2_pos = data:find( "</media:title>", title_tag1_end + 1, true )
+		video_title = data:sub( title_tag1_end + 1, title_tag2_pos - 1 )
+		video_title = ( video_title ~= nil and video_title ~= "" ) and video_title or "<unknown_title>"
+	end
+
+	return video_title
+end
+
+--[[
+	This function attempts to find and return the video length in data from an HTTP request for YouTube video info.
+	Returns nil on failure.
+]]
+function ulx.getVideoLengthFromHTTPData( data )
+
+	if data:len() <= 0 then return end
+
+	local _, dur_start = data:find( "<yt:duration seconds='", 1, true )
+	local dur_end = data:find( "'/>", dur_start + 1, true )
+
+	if not dur_start or not dur_end then return end
+
+	local duration = tonumber( data:sub( dur_start + 1, dur_end - 1 ) )
+
+	return duration
+end
+
+function ulx.play_video( calling_ply, url )
+
+	local video_id = ulx.getVideoIDFromURL( url )
+	if not video_id then
+		ULib.tsayError( calling_ply, "Invalid YouTube URL", true )
+		return
+	end
+
+	for k,v in ipairs( player.GetAll() ) do
+		current_vid[ v ] = video_id -- For saving videos to the server's playlist
+	end
+
+	umsg.Start( "ulx_playvid" )
+		umsg.String( video_id )
+	umsg.End()
+
+	http.Get( "http://gdata.youtube.com/feeds/api/videos/" .. video_id .. "?v=2", "", function( contents )
+
+		local video_title = ulx.getVideoTitleFromHTTPData( contents )
+
+		ulx.fancyLogAdmin( calling_ply, "#A played YouTube video:\n#s\n#s", url, video_title )
+		ULib.tsay( nil, "Type !stopvid to stop it.", true )
+		ULib.csay( nil, "Now playing: " .. video_title, nil, 10 )
+
+	end )
+
+end
+local play_video = ulx.command( ulx_cmd_category, "ulx playvid", ulx.play_video, "!playvid", true )
+play_video:defaultAccess( ULib.ACCESS_ADMIN )
+play_video:addParam{ type = ULib.cmds.StringArg, hint = "url" }
+play_video:help( "Plays audio from a YouTube video on all clients." )
+
+function ulx.play_video_client( calling_ply, url )
+
+	local video_id = ulx.getVideoIDFromURL( url )
+	if not video_id then
+		ULib.tsayError( calling_ply, "Invalid YouTube URL", true )
+		return
+	end
+
+	current_vid[ calling_ply ] = video_id
+
+	umsg.Start( "ulx_playvid", calling_ply )
+		umsg.String( video_id )
+	umsg.End()
+
+	http.Get( "http://gdata.youtube.com/feeds/api/videos/" .. video_id .. "?v=2", "", function( contents )
+
+		local video_title = ulx.getVideoTitleFromHTTPData( contents )
+
+		ulx.fancyLog( { calling_ply }, "#P played YouTube video client-side:\n#s\n#s", calling_ply, url, video_title ) -- Logging this way makes it only echo to the person who ran the command.
+		ULib.tsay( calling_ply, "Type !stopvid to stop it.", true )
+		ULib.csay( calling_ply, "Now playing: " .. video_title, nil, 10 )
+
+	end )
+
+end
+local play_video_client = ulx.command( ulx_cmd_category, "ulx playvidcl", ulx.play_video_client, "!playvidcl", true )
+play_video_client:defaultAccess( ULib.ACCESS_ALL )
+play_video_client:addParam{ type = ULib.cmds.StringArg, hint = "url" }
+play_video_client:help( "Plays audio from a YouTube video client-side." )
+
+function ulx.stop_video_all( calling_ply, url )
+	umsg.Start( "ulx_stopvid" )
+	umsg.End()
+	ulx.fancyLogAdmin( calling_ply, "#A stopped the YouTube video" )
+end
+local stop_video_all = ulx.command( ulx_cmd_category, "ulx stopvidall", ulx.stop_video_all, "!stopvidall", true )
+stop_video_all:defaultAccess( ULib.ACCESS_ADMIN )
+stop_video_all:help( "Stops the YouTube video that's playing for everyone." )
+
+function ulx.stop_video_client( calling_ply, url )
+	umsg.Start( "ulx_stopvid", calling_ply )
+	umsg.End()
+	ulx.fancyLog( { calling_ply }, "#P stopped the YouTube video client-side", calling_ply )
+end
+local stop_video_client = ulx.command( ulx_cmd_category, "ulx stopvid", ulx.stop_video_client, "!stopvid", true )
+stop_video_client:defaultAccess( ULib.ACCESS_ALL )
+stop_video_client:help( "Stops the YouTube video that's playing client-side." )
+
+local songplayer_video_file = "ulx/songplayer_videos.txt"
+function ulx.save_video( calling_ply, url )
+
+	local video_id
+	if url then -- Check if they're specifying a specific video to add by URL.
+
+		video_id = ulx.getVideoIDFromURL( url )
+		if not video_id then
+			ULib.tsayError( calling_ply, "Invalid YouTube URL", true )
+			return
+		end
+
+	else -- No URL specified, they must be trying to add their currently playing video.
+
+		if not current_vid[ calling_ply ] then
+			ULib.tsayError( calling_ply, "No YouTube videos have been played since you joined", true )
+			return
+		end
+		video_id = current_vid[ calling_ply ]
+		url = "http://www.youtube.com/watch?v=" .. video_id
+
+	end
+
+	if ulx.songplayer_videos[ video_id ] then
+		ULib.tsayError( calling_ply, "This video is already on the server's playlist", true )
+		return
+	end
+
+	http.Get( "http://gdata.youtube.com/feeds/api/videos/" .. video_id .. "?v=2", "", function( contents )
+
+		local video_title = ulx.getVideoTitleFromHTTPData( contents )
+
+		local video_length = ulx.getVideoLengthFromHTTPData( contents )
+		ulx.songplayer_videos[ video_id ] = { title = video_title, length = video_length }
+
+		file.Write( songplayer_video_file, ULib.makeKeyValues( ulx.songplayer_videos ) )
+
+		ULXClientSongListRefresh()
+
+
+		ulx.fancyLogAdmin( calling_ply, "#A saved YouTube video to server:\n#s\n#s", url, video_title )
+
+	end )
+
+end
+local save_video = ulx.command( ulx_cmd_category, "ulx savevid", ulx.save_video, "!savevid", true )
+save_video:defaultAccess( ULib.ACCESS_ADMIN )
+save_video:addParam{ type = ULib.cmds.StringArg, hint = "url" }
+save_video:help( "Saves a YouTube video URL to the server's playlist." )
+local save_cur_video = ulx.command( ulx_cmd_category, "ulx savecurvid", ulx.save_video, "!savecurvid", true )
+save_cur_video:defaultAccess( ULib.ACCESS_ADMIN )
+save_cur_video:help( "Saves the currently playing/last played YouTube video to the server's playlist." )
+
+function ulx.remove_video( calling_ply, url )
+
+	local video_id = ulx.getVideoIDFromURL( url )
+	if not video_id then
+		ULib.tsayError( calling_ply, "Invalid YouTube URL", true )
+		return
+	end
+
+	if not ulx.songplayer_videos[ video_id ] then
+		ULib.tsayError( calling_ply, "This video is not on the server's playlist", true )
+		return
+	end
+
+	local video_title = ulx.songplayer_videos[ video_id ].title
+
+	ulx.songplayer_videos[ video_id ] = nil
+
+	file.Write( songplayer_video_file, ULib.makeKeyValues( ulx.songplayer_videos ) )
+
+	ULXClientSongListRefresh()
+
+
+	ulx.fancyLogAdmin( calling_ply, "#A removed YouTube video from server:\n#s\n#s", url, video_title )
+
+end
+local remove_video = ulx.command( ulx_cmd_category, "ulx removevid", ulx.remove_video, "!removevid", true )
+remove_video:defaultAccess( ULib.ACCESS_ADMIN )
+remove_video:addParam{ type = ULib.cmds.StringArg, hint = "url" }
+remove_video:help( "Removes a YouTube video URL from the server's playlist." )
+
+function ulx.rename_video( calling_ply, url, new_title )
+
+	local video_id = ulx.getVideoIDFromURL( url )
+	if not video_id then
+		ULib.tsayError( calling_ply, "Invalid YouTube URL", true )
+		return
+	end
+
+	if not ulx.songplayer_videos[ video_id ] then
+		ULib.tsayError( calling_ply, "This video is not on the server's playlist", true )
+		return
+	end
+
+	local old_title = ulx.songplayer_videos[ video_id ].title
+	new_title = new_title:gsub( "<quote>", "\"" ) -- This is to fix quotation marks being replaced with ' by RunConsoleCommand.
+	ulx.songplayer_videos[ video_id ].title = new_title
+
+	file.Write( songplayer_video_file, ULib.makeKeyValues( ulx.songplayer_videos ) )
+
+	ULXClientSongListRefresh()
+
+
+	ulx.fancyLogAdmin( calling_ply, "#A renamed YouTube video from:\n#s\nto:\n#s", old_title, new_title )
+
+end
+local rename_video = ulx.command( ulx_cmd_category, "ulx renamevid", ulx.rename_video, "!renamevid", true )
+rename_video:defaultAccess( ULib.ACCESS_ADMIN )
+rename_video:addParam{ type = ULib.cmds.StringArg, hint = "url" }
+rename_video:addParam{ type = ULib.cmds.StringArg, hint = "title", ULib.cmds.takeRestOfLine }
+rename_video:help( "Renames a video on the server's playlist." )
+
+function ulx.music( calling_ply )
+	ULib.clientRPC( calling_ply, "ULXOpenMusicPlayer" )
+end
+local music = ulx.command( ulx_cmd_category, "ulx music", ulx.music, "!music", true )
+music:defaultAccess( ULib.ACCESS_ALL )
+music:help( "Opens the YouTube music player menu." )
+
+if SERVER then
+
+if not ulx.songplayer_videos then
+	ulx.songplayer_videos = {}
+	if file.Exists( songplayer_video_file ) then
+		ulx.songplayer_videos = ULib.parseKeyValues( file.Read( songplayer_video_file ) )
+	end
+end
+
+local function internalCommandForMenu( calling_ply, cmd, args )
+	local arg = args[ 1 ]
+	if arg == "playvid" or arg == "playvidcl" then
+
+		if not ULib.ucl.query( calling_ply, "ulx " .. arg ) then
+			ULib.tsay( calling_ply, "You do not have access to this command, " .. calling_ply:Nick() .. ".", true )
+			return
+		end
+
+		local video_id = args[ 2 ]
+		if not video_id or video_id:len() ~= 11 then
+			ULib.tsayError( calling_ply, "Invalid video ID", true )
+			return
+		end
+
+		if not ulx.songplayer_videos[ video_id ] then
+			ULib.tsayError( calling_ply, "This video is not on the server's playlist", true )
+			return
+		end
+
+		local target = ( arg == "playvidcl" ) and calling_ply or nil
+
+		umsg.Start( "ulx_playvid", target )
+			umsg.String( video_id )
+		umsg.End()
+
+		local video_title = ulx.songplayer_videos[ video_id ].title
+
+		if target then -- Playing client-side
+			ulx.fancyLog( { calling_ply }, "#P played YouTube video client-side:\n#s", calling_ply, video_title )
+		else
+			ulx.fancyLogAdmin( calling_ply, "#A played YouTube video:\n#s", video_title )
+		end
+
+		ULib.tsay( target, "Type !stopvid to stop it.", true )
+		ULib.csay( target, "Now playing: " .. video_title, nil, 10 )
+
+		if target then
+			current_vid[ target ] = video_id
+		else
+			for k,v in ipairs( player.GetAll() ) do
+				current_vid[ v ] = video_id
+			end
+		end
+
+	elseif arg == "retrievesongs" and ULib.ucl.query( calling_ply, "ulx music" ) then
+		for video_id, video_table in pairs( ulx.songplayer_videos ) do
+			umsg.Start( "ULXTransferSong", calling_ply )
+				umsg.String( video_id )
+				umsg.String( video_table.title )
+				umsg.Long( video_table.length )
+			umsg.End()
+		end
+		umsg.Start( "ULXTransferSongsDone", calling_ply )
+		umsg.End()
+	end
+end
+concommand.Add( "_ulx_songplayer", internalCommandForMenu, nil, "*DO NOT RUN DIRECTLY* Internal command for the ULX YouTube music player menu." )
+
+function ULXClientSongListRefresh( ply ) -- Makes a client (or all clients) refresh their song list.
+	ULib.clientRPC( ply, "ULXMusicPlayerRefresh" )
+end
+
+local function removePlyCurVid( ent )
+	if ValidEntity( ent ) and ent:GetClass() == "player" then
+		current_vid[ ent ] = nil
+	end
+end
+hook.Add( "EntityRemoved", "ulxRemovePlyCurVid", removePlyCurVid ) -- I read that PlayerDisconnected sometimes doesn't get called, so I'm using EntityRemoved instead.
+
+end
